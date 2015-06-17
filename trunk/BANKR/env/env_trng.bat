cb938ce5ed135566548f9e52f8166ead954c165e
uDoi <uDoi@2364f4dd-7815-ee4c-b9ea-42ae821c1292>
2015-06-17 10:23:38 +0000

    【1.0】1.0次SVNコミット漏れ(現地用設定ファイル）
    git-svn-id: http://10.16.14.243:13000/svn/azbBatch@121 2364f4dd-7815-ee4c-b9ea-42ae821c1292

--- trunk/BANKR/env/env_trng.bat
+++ trunk/BANKR/env/env_trng.bat
@@ -5,31 +5,30 @@ rem ============================================================================
 rem 
 rem  SYSTEM        : BANK・R－CC
 rem 
-rem  PROGRAM NAME  : 環境変数定義ファイル(AZBK)
+rem  PROGRAM NAME  : 環境変数定義ファイル(TRAINING)
 rem 
-rem  MODULE NAME   : env_azbk.bat
+rem  MODULE NAME   : env_trng.bat
 rem 
 rem 
 rem  Mod   yy/mm/dd   Coder           Comment
 rem -----+----------+---------------+-------------------------------------------
 rem  %00 | 14/05/14 | ISID          | First Edition.
-rem  %01 | 15/03/30 | 土居 康一郎   | 1.0次対応 DB環境、パス名を変更
-rem  %02 | 15/06/02 | 土居 康一郎   | 1.0次対応 パスワード暗号化修正
+rem  %01 | 15/06/18 | 土居 康一郎   | 1.0次対応 研修用に修正
 rem ============================================================================
 
 
 rem ---------------------------------------
 rem  環境変数のセット
 rem ---------------------------------------
-set DB_NAME=BANKRAZB
+set DB_NAME=BANKRTRN
 set EMB_HOME=E:\azbk\BANKRAZB
 set HOME=E:\azbk\BANKRAZB
 set LOG_DIR=E:\azbk\BANKRAZB\log
 set MBENV_KOKYAKU_KEY=CIF
 set ORACLE_HOME=D:\oracle\product\11.2.0\dbhome_1
-set ORACLE_SID=BANKRAZB
-set TBS_NAME_EFF_IDX=EMBIHISTORY
-set TBS_NAME_EFF_TBL=EMBTHISTORY
-set USER=BATCH
-set DB_UID=spv
-set DB_PWD=eaux
+set ORACLE_SID=BANKRTRN
+set TBS_NAME_EFF_IDX=EMBIDX
+set TBS_NAME_EFF_TBL=EMBDATA
+set USER=TRAINING
+set DB_UID=training
+set DB_PWD=ipwrwrjac
