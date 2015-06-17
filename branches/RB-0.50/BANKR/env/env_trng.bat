a382a81a63974a859f391aa8b14e34e44112c4b6
uDoi <uDoi@2364f4dd-7815-ee4c-b9ea-42ae821c1292>
2015-06-17 09:38:42 +0000

    【0.5】0.5次SVNコミット漏れ(研修用設定ファイル）
    git-svn-id: http://10.16.14.243:13000/svn/azbBatch@120 2364f4dd-7815-ee4c-b9ea-42ae821c1292

---
+++ branches/RB-0.50/BANKR/env/env_trng.bat
@@ -0,0 +1,33 @@
+@echo off
+rem ============================================================================
+rem  Copyright　Information Services International-Dentsu, Ltd.
+rem ============================================================================
+rem 
+rem  SYSTEM        : BANK・R－CC
+rem 
+rem  PROGRAM NAME  : 環境変数定義ファイル(TRAINING)
+rem 
+rem  MODULE NAME   : env_trng.bat
+rem 
+rem 
+rem  Mod   yy/mm/dd   Coder           Comment
+rem -----+----------+---------------+-------------------------------------------
+rem  %00 | 14/05/14 | ISID          | First Edition.
+rem ============================================================================
+
+
+rem ---------------------------------------
+rem  環境変数のセット
+rem ---------------------------------------
+set DB_NAME=BRCCTRNG
+set EMB_HOME=E:\azbk\BANKR
+set HOME=E:\azbk\BANKR
+set LOG_DIR=E:\azbk\BANKR\log
+set MBENV_KOKYAKU_KEY=CIF
+set ORACLE_HOME=D:\oracle\product\11.2.0\dbhome_1
+set ORACLE_SID=BRCCTRNG
+set TBS_NAME_EFF_IDX=EMBIHISTORY
+set TBS_NAME_EFF_TBL=EMBTHISTORY
+set USER=TRAINING
+set DB_UID=training
+set DB_PWD=ovcxcxpgi
