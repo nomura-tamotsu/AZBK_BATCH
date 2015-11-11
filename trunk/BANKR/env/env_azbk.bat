ace122a2da505b3065109e742a8213a53eaeddb5
uKawashima <uKawashima@2364f4dd-7815-ee4c-b9ea-42ae821c1292>
2015-11-11 08:31:54 +0000

    【1.0】クラスタ対応　DB接続先変更
    git-svn-id: http://10.16.14.243:13000/svn/azbBatch@132 2364f4dd-7815-ee4c-b9ea-42ae821c1292

--- trunk/BANKR/env/env_azbk.bat
+++ trunk/BANKR/env/env_azbk.bat
@@ -15,19 +15,20 @@ rem -----+----------+---------------+-------------------------------------------
 rem  %00 | 14/05/14 | ISID          | First Edition.
 rem  %01 | 15/03/30 | 土居 康一郎   | 1.0次対応 DB環境、パス名を変更
 rem  %02 | 15/06/02 | 土居 康一郎   | 1.0次対応 パスワード暗号化修正
+rem  %03 | 15/11/11 | 川島 直樹     | 1.0次対応 DB接続先変更（クラスタ対応）
 rem ============================================================================
 
 
 rem ---------------------------------------
 rem  環境変数のセット
 rem ---------------------------------------
-set DB_NAME=BANKRAZB
+set DB_NAME=BRCCAZBK
 set EMB_HOME=E:\azbk\BANKRAZB
 set HOME=E:\azbk\BANKRAZB
 set LOG_DIR=E:\azbk\BANKRAZB\log
 set MBENV_KOKYAKU_KEY=CIF
 set ORACLE_HOME=D:\oracle\product\11.2.0\dbhome_1
-set ORACLE_SID=BANKRAZB
+set ORACLE_SID=BRCCAZBK
 set TBS_NAME_EFF_IDX=EMBIHISTORY
 set TBS_NAME_EFF_TBL=EMBTHISTORY
 set USER=BATCH
--- trunk/BANKR/env/env_trng.bat
+++ trunk/BANKR/env/env_trng.bat
@@ -14,19 +14,20 @@ rem  Mod   yy/mm/dd   Coder           Comment
 rem -----+----------+---------------+-------------------------------------------
 rem  %00 | 14/05/14 | ISID          | First Edition.
 rem  %01 | 15/06/18 | 土居 康一郎   | 1.0次対応 研修用に修正
+rem  %02 | 15/11/11 | 川島 直樹     | 1.0次対応 DB接続先変更（クラスタ対応）
 rem ============================================================================
 
 
 rem ---------------------------------------
 rem  環境変数のセット
 rem ---------------------------------------
-set DB_NAME=BANKRTRN
+set DB_NAME=BRCCTRNG
 set EMB_HOME=E:\azbk\BANKRAZB
 set HOME=E:\azbk\BANKRAZB
 set LOG_DIR=E:\azbk\BANKRAZB\log
 set MBENV_KOKYAKU_KEY=CIF
 set ORACLE_HOME=D:\oracle\product\11.2.0\dbhome_1
-set ORACLE_SID=BANKRTRN
+set ORACLE_SID=BRCCTRNG
 set TBS_NAME_EFF_IDX=EMBIDX
 set TBS_NAME_EFF_TBL=EMBDATA
 set USER=TRAINING
